6a7e09631f9c73eef881cd021c6c72dffeb4c21f
codewithsaidul <codewithsaidul@gmail.com>
2025-09-22 23:33:55 +0600

    fetch product data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <h1 className="text-7xl text-rose-500">My Nextjs Blog</h1>
 
-      <div>
+      {/* <div>
         <h2 className="text-2xl font-bod mb-5">Normal Image</h2>
         <img
           src="https://images.pexels.com/photos/5615067/pexels-photo-5615067.jpeg"
@@ -14,7 +14,7 @@ export default function Home() {
           height={500}
           className="mx-auto"
         />
-      </div>
+      </div> */}
 
       <div>
         <h2 className="text-2xl font-bod mb-5">Next Optimized Image</h2>
---
+++ src/app/products/page.tsx
@@ -0,0 +1,15 @@
+
+async function page  () {
+    const res = await fetch("http://localhost:5000/products");
+    const products = await res.json()
+    console.log(products)
+  return (
+    <div>
+       <h1>All Products Page</h1>
+   </div>
+  );
+};
+
+
+
+export default page
