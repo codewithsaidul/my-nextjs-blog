8c03591dfaae9e1fcf0cc99ae86af163d57992d5
codewithsaidul <codewithsaidul@gmail.com>
2025-09-23 23:35:16 +0600

    add loading spinner while fetching data in ssr mode

---
+++ src/app/products/loading.tsx
@@ -0,0 +1,8 @@
+
+export default function ProductLoading () {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+       <h1 className="text-6xl">Loading</h1>
+   </div>
+  );
+};
